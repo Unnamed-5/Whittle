a46635616abb8fb50af555b4544efb2bd3099e64
R <51994785+Unnamed-5@users.noreply.github.com>
2021-10-21 09:19:16 +0800

    Update Canvas+Bluetooth.swift

--- IvyControl/IvyControl/Canvas+Bluetooth.swift
+++ IvyControl/IvyControl/Canvas+Bluetooth.swift
@@ -139,7 +139,7 @@ extension CanvasViewController: CBCentralManagerDelegate, CBPeripheralDelegate {
             presentCompleteAlert()
         } else {
             print(error!)
-            presentErrorAlert()()
+            presentErrorAlert()
         }
     }
     
